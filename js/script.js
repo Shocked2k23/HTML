b7d4ae43b55a6ea3ac03417d5e3c94c5b08d5d22
Shocked2k23 <rantanenonni39@gmail.com>
2024-09-09 13:44:57 +0300

    lumoakatemia t1-t13

---
+++ js/script.js
@@ -0,0 +1,90 @@
+/*tehtävä 1*/
+
+console.log("Hei maailma");
+
+/*tehtävä 2*/
+
+var luku1, luku2, summa;
+luku1 = 4
+luku2 = 6
+summa = luku1 + luku2
+console.log("Lukujen summa on: " + summa);
+
+/*tehtävä 3*/
+
+var luku1, luku2, summa;
+luku1 = 10
+luku2 = 20
+console.log(luku1 - luku2);
+
+/*tehtävä 4*/
+
+var luku1, luku2, summa;
+luku1 = 15
+luku2 = 20
+summa = luku1 * luku2
+console.log(luku1 * luku2);
+
+/*tehtävä 5*/
+
+var luku1, luku2, summa;
+luku1 = 10
+luku2 = 20
+summa = luku1 / luku2
+console.log(luku1 / luku2);
+
+/*tehtävä 6*/
+
+var cel, fah;
+cel = 25
+fah = (cel * 9/5) + 32
+console.log(fah);
+
+/*tehtävä 7*/
+
+var cel, fah;
+fah = 77
+cel = (fah - 32) * 5/9
+console.log(cel);
+
+/*tehtävä 8*/
+
+var kilometers, miles;
+kilometers = 10
+miles = kilometers * 0.621371
+console.log(miles);
+
+/*tehtävä 9*/
+
+var kilometers, miles;
+miles = 5
+kilometers = miles * 1.60934
+console.log(kilometers);
+
+/*tehtävä 10*/
+
+var tuuma, sentti;
+tuuma = 10
+sentti = tuuma * 2.54
+console.log(sentti);
+
+/*tehtävä 11*/
+
+var tuuma, sentti;
+sentti = 75
+tuuma = sentti / 2.54
+console.log(tuuma);
+
+/*tehtävä 12*/
+
+var yard, metri;
+yard = 10
+metri = yard * 0.9144
+console.log(metri);
+
+/*tehtävä 13*/
+
+var yard, metri;
+metri = 5
+yard = metri / 0.9144
+console.log(yard);
