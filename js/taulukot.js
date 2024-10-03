aef008fe4dfdf8eca116ad03f82ee59c0a848aa6
Shocked2k23 <rantanenonni39@gmail.com>
2024-10-03 09:29:39 +0300

    validpassword

---
+++ js/taulukot.js
@@ -0,0 +1,45 @@
+/*let kauppalista = [];
+kauppalista.push("peruna");
+kauppalista.push("makkara");
+kauppalista.push("mehu");
+
+console.log(kauppalista)
+
+let nimet = ["Juuso", "Pekka", "Juha", "Sami", "Kermit"];
+
+console.log(nimet)
+
+let numerot = [1, 2, 3, 4, 5];
+let pituus = numerot.length;
+
+console.log(pituus)
+
+let nimet = ["Matti", "Pekka", "Vesa"];
+nimet.push("Antti");
+
+console.log(nimet)
+
+let taulukko1 = [2, 5, 6];
+let taulukko2 = [1, 7, 8];
+let taulukot = taulukko1.concat(taulukko2);
+
+console.log(taulukot)
+
+let numerot = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let parillisetNumerot = numerot.filter(numero => numero % 2 === 0);
+console.log(parillisetNumerot);
+
+// Toimii mulla ainakin ilman numerot.sort((a, b) => a - b);
+let numerot = [5, 2, 9, 1, 5, 8, 3];
+numerot.sort();
+console.log(numerot);
+
+let numerot = [5, 2, 9, 1, 5, 8, 3];
+numerot.reverse();
+console.log(numerot);
+
+*/
+
+const nimet = ["Matti", "Anna", "Eeva"];
+const merkkijono = nimet.join(", ");
+console.log(merkkijono);
---
+++ lomake-harjoitus/signup-form/script.js
@@ -0,0 +1,36 @@
+function validateForm(){
+
+    let fname = document.getElementById("fname").value
+    let lname = document.getElementById("lname").value
+    let birthDate = document.getElementById("birth-date").value
+    let gender = document.getElementsByName("gender").value
+    let country = document.getElementById("country").value
+    let email = document.getElementById("email").value
+    let phone = document.getElementById("phone").value
+    let password = document.getElementById("password1").value
+    let confirmpassword = document.getElementById("password2").value
+    let tsu = document.getElementById("tsu").value
+    let validpassword = false
+
+    if (password !== confirmpassword){
+        prompt("Passwords don't match")
+        validpassword = false
+    } else {
+        validpassword = true
+    }
+
+    if (fname.trim() == ""){
+        prompt("Type valid First Name")
+    } else if (lname.trim() == ""){
+        prompt("Type valid Last Name")
+    } else if (email.trim() == ""){
+        prompt("Type valid Email Address")
+    } else if (phone.trim() == ""){
+        prompt("Type valid Phone Number")
+    } else if (password.trim() == "" && validpassword == false){
+        prompt("Password can't be empty")
+    } else if (confirmpassword.trim() == "" && validpassword == false){
+        prompt("Password can't be empty or doesn't match")
+    }
+
+}
